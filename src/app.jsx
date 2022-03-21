64fd5ae6ee2318d60d801569fa7ad6a5613a0ccd
jixindong <it_jxd@foxmail.com>
2022-03-21 10:53:24 +0800

    chore: 🔧 全局配置修改

--- src/app.jsx
+++ src/app.jsx
@@ -1,22 +1,22 @@
-import { SettingDrawer } from '@ant-design/pro-layout';
-import { PageLoading } from '@ant-design/pro-layout';
+// 全局配置
+// https://umijs.org/zh-CN/config
 import { history, Link } from 'umi';
-import RightContent from '@/components/RightContent';
-import Footer from '@/components/Footer';
-import { currentUser as queryCurrentUser } from './services/ant-design-pro/api';
 import { BookOutlined, LinkOutlined } from '@ant-design/icons';
+import { PageLoading, SettingDrawer } from '@ant-design/pro-layout';
+
+import { currentUser as queryCurrentUser } from './services/ant-design-pro/api';
 import defaultSettings from '../config/defaultSettings';
+
 const isDev = process.env.NODE_ENV === 'development';
 const loginPath = '/user/login';
-/** 获取用户信息比较慢的时候会展示一个 loading */
 
-export const initialStateConfig = {
-  loading: <PageLoading />,
-};
+// 获取用户信息比较慢的时候会展示一个 loading
+export const initialStateConfig = { loading: <PageLoading /> };
+
 /**
- * @see  https://umijs.org/zh-CN/plugins/plugin-initial-state
- * */
-
+ * 运行时配置 
+ * ProLayout 支持的api https://procomponents.ant.design/components/layout
+ */
 export async function getInitialState() {
   const fetchUserInfo = async () => {
     try {
@@ -42,16 +42,14 @@ export async function getInitialState() {
     fetchUserInfo,
     settings: defaultSettings,
   };
-} // ProLayout 支持的api https://procomponents.ant.design/components/layout
+};
 
 export const layout = ({ initialState, setInitialState }) => {
   return {
-    rightContentRender: () => <RightContent />,
+    rightContentRender: () => false,
     disableContentMargin: false,
-    waterMarkProps: {
-      content: initialState?.currentUser?.name,
-    },
-    footerRender: () => <Footer />,
+    waterMarkProps: { content: initialState?.currentUser?.name },
+    footerRender: () => false,
     onPageChange: () => {
       const { location } = history; // 如果没有登录，重定向到 login
 
@@ -61,15 +59,15 @@ export const layout = ({ initialState, setInitialState }) => {
     },
     links: isDev
       ? [
-          <Link to="/umi/plugin/openapi" target="_blank">
-            <LinkOutlined />
-            <span>OpenAPI 文档</span>
-          </Link>,
-          <Link to="/~docs">
-            <BookOutlined />
-            <span>业务组件文档</span>
-          </Link>,
-        ]
+        <Link to="/umi/plugin/openapi" target="_blank">
+          <LinkOutlined />
+          <span>OpenAPI 文档</span>
+        </Link>,
+        <Link to="/~docs">
+          <BookOutlined />
+          <span>业务组件文档</span>
+        </Link>,
+      ]
       : [],
     menuHeaderRender: undefined,
     // 自定义 403 页面
